85574244178933968196fbc506b830be7f8a5f1c
KoutaYamazawa <kouta0810277@gmail.com>
2018-03-21 21:26:23 +0900

    最終課題　カーリングゲーム
    ドラッグ挙動成功

---
+++ Assets/BallControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControll : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//クリックしたら、Stoneが付いて来る
+        //引っ張る長さによって強さを付ける。
+        //ぶつかったら、跳ね返るか、留まる。
+	}
+}
---
+++ Assets/Stone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnMouseDrag();
+        }
+
+        if (Input.GetMouseButton(0) == false)
+        {
+
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 objectPointInScreen
+            = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        Vector2 mousePointInScreen = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        Vector2 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+        this.transform.position = mousePointInWorld;
+    }
+}
